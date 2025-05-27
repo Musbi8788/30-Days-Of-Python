a5edcad9d0d0d2b28f1f25e60424f6e10a648664
Musbi <musbi700@gmail.com>
2025-05-27 13:04:18 -0700

    Update helloworld.py

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,8 +1,8 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
 
-print(3 + 2)   # addition(+)
-print(3 - 2)   # subtraction(-)
+print(3 + 5)   # addition(+)
+print(3 - 1)   # subtraction(-)
 print(3 * 2)   # multiplication(*)
 print(3 / 2)   # division(/)
 print(3 ** 2)  # exponential(**)
@@ -16,7 +16,7 @@ print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
 print(type('Asabeneh'))          # String
 print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
+print(type({'name':'Musbi'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
 print(type(3 == 3))              # Bool
